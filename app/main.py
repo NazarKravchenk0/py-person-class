97fa4014bda5852d7c152cd12cbcbf529a77f5a8
nazarkravchenko <nazarnazarik5@gmail.com>
2025-08-26 22:41:47 +0300

    check

--- app/main.py
+++ app/main.py
@@ -8,17 +8,20 @@ class Person:
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
-    # create Person instances
     persons = [
         Person(person_dict["name"], person_dict["age"])
         for person_dict in people
     ]
 
-    # link wife/husband attributes
     for person_dict in people:
         person = Person.people[person_dict["name"]]
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            setattr(person, "wife", Person.people[person_dict["wife"]])
-        if "husband" in person_dict and person_dict["husband"] is not None:
-            setattr(person, "husband", Person.people[person_dict["husband"]])
+
+        wife_name = person_dict.get("wife")
+        if wife_name:
+            setattr(person, "wife", Person.people[wife_name])
+
+        husband_name = person_dict.get("husband")
+        if husband_name:
+            setattr(person, "husband", Person.people[husband_name])
+
     return persons
